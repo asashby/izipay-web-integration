8475ca4351a4e948cb042134bae033bd7d3822b9
Ashby Hernandez <asashby@hotmail.com>
2021-11-26 18:34:27 -0400

    testing fetching

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import {React, useState} from 'react';
+
+
 
 function Body() {
+	//const [token, setToken] = React.useState(null);
 
+    const getFormToken = async () =>{
+        const requestOptions = {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ODQ1NjI4MzM6cHJvZHBhc3N3b3JkX1MwQ2lGMHhNUUowOG9oVVVFR282U3BZajZ2QUVxYWVkbFQ5aG1mV1JLVmtJbg=='
+            },
+            body: JSON.stringify({ 
+                amount: 180,
+                currency: 'PEN',
+                orderId: 'myOrderId-999999',
+                customer: {
+                    email: 'sample@example.com'
+                }
+            })
+        };
+        const response = fetch('https://api.micuentaweb.pe/api-payment/V4/Charge/CreatePayment', requestOptions)
+                            .then(response => response.json())
+                            .then(data => this.setState({ postId: data.id }));
+
+        console.log(response);
+
+        /*const json = JSON.parse(response);
+        setToken(
+            json.answer.formToken
+        );  */
+    }
+    
+    getFormToken();
+    
     return (
         <div
             style={{
@@ -9,7 +42,7 @@ function Body() {
             }}>
             <div 
                 class="kr-embedded" 
-                kr-form-token="">
+                kr-form-token="{token}">
             
                 <div class="kr-pan"></div>
                 <div class="kr-expiry"></div>
